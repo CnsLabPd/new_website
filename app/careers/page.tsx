433f5c828e16dda83050d4c315b0a796f4dd646d
CnsLabPd <cnspdapp@gmail.com>
2025-07-09 21:00:26 +0530

    Update page.tsx

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -57,7 +57,7 @@ export default function CareersPage() {
         <div className="flex items-center gap-2">
           <Link href="/">
             <div className="flex items-center gap-2">
-              <img src="/images/neurogati-logo-new.jpg" alt="Neurogati Logo" className="h-8 w-8 object-contain" />
+              <img src="/images/Neurogati.jpg" alt="Neurogati Logo" className="h-8 w-8 object-contain" />
               <span className="text-xl font-bold text-white">Neurogati</span>
             </div>
           </Link>
