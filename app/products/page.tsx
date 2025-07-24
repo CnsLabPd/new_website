0ec8de9fe092455d05205398f3b688c8d53c34f8
Shrinivas Sesadri <shrini1908@gmail.com>
2025-07-25 01:08:32 +0530

    Update banner

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -9,7 +9,7 @@ export default function ProductsPage() {
   return (
     <div className="flex min-h-screen flex-col">
       <div className="relative flex w-full flex-col overflow-y-auto">
-        <StickyBanner className="bg-gradient-to-b from-blue-500 to-blue-600">
+        <StickyBanner className="bg-gradient-to-b from-cyan-500 to-blue-500 hover:from-cyan-600 hover:to-blue-600">
           <p className="mx-0 max-w-[90%] text-white drop-shadow-md">
             Announcing Workshop on AI applications in EEG from September 12th to 14th, 2025.{" "}
             <a href="https://sites.google.com/smail.iitm.ac.in/eegaiworkshop/home?authuser=0" className="transition duration-200 hover:underline">
