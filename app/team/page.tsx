87e538963055268b0277fb48e6fd26d0b11dcf19
CnsLabPd <cnspdapp@gmail.com>
2025-10-24 15:47:42 +0530

    Update page.tsx

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -10,8 +10,8 @@ export default function TeamPage() {
       <div className="relative flex w-full flex-col overflow-y-auto">
         <StickyBanner className="bg-gradient-to-b from-cyan-500 to-blue-500 hover:from-cyan-600 hover:to-blue-600">
           <p className="mx-0 max-w-[90%] text-white drop-shadow-md">
-            Announcing Workshop on AI applications in EEG from September 12th to 14th, 2025.{" "}
-            <a href="https://sites.google.com/smail.iitm.ac.in/eegaiworkshop/home?authuser=0" className="transition duration-200 hover:underline">
+            Announcing Workshop on AI applications in EEG from October 31st to Novemeber 02nd, 2025.{" "}
+            <a href="https://sites.google.com/smail.iitm.ac.in/eegaiworkshop/workshop-oct-2025/home?authuser=0" className="transition duration-200 hover:underline">
               Visit Website
             </a>
           </p>
